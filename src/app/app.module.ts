7e0be1a311fb0cd83e9d1422aab49a58dda5622d
Kevin Aviles <kevinaviles@MacBook-Pro-de-Kevin.local>
2017-10-09 22:04:16 -0500

    Million dollar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+//Servicios
+import {ListaDeseosService} from './services/lista-deseos.service';
+
+// Pipes
+import { PlaceHolderPipe } from './pipes/placeholder.pipe';
+import { PendientesPipe } from './pipes/pendientes.pipes';
+
+
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
+import { AgregarComponent } from '../pages/agregar/agregar.component';
+import { DetalleComponent } from '../pages/detalle/detalle.component';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PlaceHolderPipe,
+    PendientesPipe,
+    PendientesComponent,
+    TerminadosComponent,
+    PendientesComponent,
+    AgregarComponent,
+    DetalleComponent,
     TabsPage
   ],
   imports: [
@@ -26,14 +38,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PendientesComponent,
+    TerminadosComponent,
+    PendientesComponent,
+    AgregarComponent,
+    DetalleComponent,
     TabsPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
+    ListaDeseosService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
